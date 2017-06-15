a27cec1efb419a8cfa6787bdea547255890342ce
Alex Cooke <alex@seers.com.au>
2017-06-15 17:29:37 +1000

    Fix e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-cli-sampler App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toContain('Welcome to Angular CLI Sampler');
   });
 });
--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users: any[];
+  hasPermission: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    
+    if (this.hasPermission) {
+
+      this.getUserss()
+        .then(users => this.users = users)
+        .catch(e => console.log(e.message));
+
+    } else {
+
+      this.users = [];
+      
+    }
+
+  }
+
+  async getUserss() {
+    return [
+      { name: "john", email: 'john@angular.com' },
+      { name: "jame", email: 'jane@angular.com'}
+    ];
   }
 
 }
